7de9c58718da124bd722e5c42bde4a9babe49480
Mugesh09 <103987590+Mugesh09@users.noreply.github.com>
2026-08-01 23:22:48 +0530

    Delete HabitTracker directory

--- HabitTracker/app/src/main/java/com/example/habittracker/widget/HabitWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.example.habittracker.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class HabitWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = HabitWidget()
-}
